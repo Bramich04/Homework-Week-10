2fb5691d10adaa6502eda0acab68bad08fe0565a
Oliver Bramich <147389067+Bramich04@users.noreply.github.com>
2024-10-05 13:15:56 +1000

    Add files via upload

---
+++ Homework_W10.js
@@ -0,0 +1,11 @@
+var vg_1 = "Temperature_Visualisation.vega";
+vegaEmbed("#Temperature_Visualisation", vg_1).then(function(result) {}).catch(console.error);
+
+var vg_2 = "Bushfire_Visualisation.vega";
+vegaEmbed("#Bushfire_Visualisation", vg_2).then(function(result) {}).catch(console.error);
+
+var vg_3 = "Environment_Visualisation.vega";
+vegaEmbed("#Environment_Visualisation", vg_3).then(function(result) {}).catch(console.error);
+
+var vg_4 = "BurntArea_Visualisation.vega";
+vegaEmbed("#BurntArea_Visualisation", vg_4).then(function(result) {}).catch(console.error);
